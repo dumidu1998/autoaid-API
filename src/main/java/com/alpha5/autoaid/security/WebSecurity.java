be0e0831d3ee09d89a4aeef163020746e2e60625
dumidu1998 <dumiduraj@gmail.com>
2021-07-01 19:02:32 +0530

    jwt util file  OK

--- src/main/java/com/alpha5/autoaid/security/WebSecurity.java
+++ src/main/java/com/alpha5/autoaid/security/WebSecurity.java
@@ -3,4 +3,7 @@ package com.alpha5.autoaid.security;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 public class WebSecurity extends  WebSecurityConfigurerAdapter{
+
+
+
 }
--- src/main/java/com/alpha5/autoaid/util/JwtTokenUtil.java
+++ src/main/java/com/alpha5/autoaid/util/JwtTokenUtil.java
@@ -1,10 +1,71 @@
 package com.alpha5.autoaid.util;
 
-import java.io.Serializable;
+import com.alpha5.autoaid.model.Customer;
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import io.jsonwebtoken.io.Decoders;
+import org.springframework.stereotype.Component;
 
+import java.io.Serializable;
+import java.security.Key;
+import java.util.Date;
+
+@Component
 public class JwtTokenUtil implements Serializable {
 
+    private static final Logger logger = LoggerFactory.getLogger(JwtTokenUtil.class);
+    public static final long ACCESS_TOKEN_VALIDITY_SECONDS = 5*60*60*1000;
+    public static final String SIGNING_KEY = "aweg34y54hw54h3h135h2455444442h5245h245h25h34gre3qh4qh34t";
 
+    public static String generateToken(Customer principal) {
+        byte[] keyBytes = Decoders.BASE64.decode(SIGNING_KEY);
+        Key key = Keys.hmacShaKeyFor(keyBytes);
+        Date now = new Date();
+        Date expiryDate = new Date(now.getTime() + ACCESS_TOKEN_VALIDITY_SECONDS);
+        return Jwts.builder()
+//                .claim("username", principal.getUsername())
+//                .claim("id", principal.getId())
+//                .claim("image", principal.getImageUrl())
+                .claim("id", principal.getCustomerId())
+                .setSubject(principal.getEmail())
+                .setIssuedAt(now)
+                .setExpiration(expiryDate)
+                .signWith(key)
+                .compact();
+    }
 
+    public boolean validateToken(String jwt) {
+        try {
+            byte[] keyBytes = Decoders.BASE64.decode(SIGNING_KEY);
+            Key key = Keys.hmacShaKeyFor(keyBytes);
+            Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(jwt);
+            return true;
+        } catch (SecurityException ex) {
+            logger.error("Invalid JWT signature");
+            throw new RuntimeException("Invalid JWT signature");
+        } catch (MalformedJwtException ex) {
+            logger.error("Invalid JWT token");
+            throw new RuntimeException("Invalid JWT token\"");
+        } catch (ExpiredJwtException ex) {
+            logger.error("Expired JWT token");
+            throw new RuntimeException("Expired JWT token");
+        } catch (UnsupportedJwtException ex) {
+            logger.error("Unsupported JWT token");
+            throw new RuntimeException("Unsupported JWT token");
+        } catch (IllegalArgumentException ex) {
+            logger.error("JWT claims string is empty.");
+            throw new RuntimeException("JWT claims string is empty.");
+        }
+    }
+
+    public String getUserNameFromJwt(String jwt) {
+        byte[] keyBytes = Decoders.BASE64.decode(SIGNING_KEY);
+        Key key = Keys.hmacShaKeyFor(keyBytes);
+        Claims claims = Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(jwt).getBody();
+
+        return claims.getSubject();
+    }
 
 }
