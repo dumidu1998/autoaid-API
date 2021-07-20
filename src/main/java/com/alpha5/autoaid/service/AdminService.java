0eeb9a361407dab969044ef2081ca991a6bdaeec
AmdPathirana <2018is052@stu.ucsc.cmb.ac.lk>
2021-07-20 19:42:47 +0530

    Adminlist second round

--- src/main/java/com/alpha5/autoaid/service/AdminService.java
+++ src/main/java/com/alpha5/autoaid/service/AdminService.java
@@ -1,20 +1,40 @@
 package com.alpha5.autoaid.service;
 
+
+import com.alpha5.autoaid.dto.response.*;
+import com.alpha5.autoaid.enums.StaffRole;
 import com.alpha5.autoaid.model.Staff;
+
+
 import com.alpha5.autoaid.repository.StaffRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.stereotype.Service;
+
+import javax.swing.text.SimpleAttributeSet;
+import java.awt.font.TextHitInfo;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.SocketHandler;
 
 @Service
 public class AdminService {
-
     @Autowired
     private StaffRepository staffRepository;
-
-    private String staffRegister(Staff staff) {
-//        if(staffRepository.findByContactNoAndEmail(staff.getContactNo(),staff.getEmail())!=null){
-//            throw new RuntimeException("User email and contact already taken");
-//        }
-        return "Done";
+    //-----------_______________________--------------------ADD to TO Real ------------____________________-----//
+    //------------------Staff Handling NavBar Data------------------//
+    public List<AdminListRespond> getAdmins(){
+        List<Staff> admins= staffRepository.findAllByRole(StaffRole.ADMIN);
+        List<AdminListRespond> outlist=new ArrayList<AdminListRespond>();
+        for (Staff entity: admins){
+            AdminListRespond adminList=new AdminListRespond();
+            adminList.setFirstName(entity.getFirstName());
+            adminList.setLastname(entity.getLastName());
+            adminList.setId(entity.getStaffId());
+            outlist.add(adminList);
+        }
+        return outlist;
     }
+
 }
+
