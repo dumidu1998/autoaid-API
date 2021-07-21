1070a5f45b90ec8a692ab08c0ccb209499c65d35
Madhawa Dissanayake <73326224+MadhawaDissanayake@users.noreply.github.com>
2021-07-21 09:45:05 +0530

    files created stockKeeper

---
+++ src/main/java/com/alpha5/autoaid/service/StockService.java
@@ -0,0 +1,33 @@
+package com.alpha5.autoaid.service;
+
+import com.alpha5.autoaid.dto.response.InventryStockRespond;
+import com.alpha5.autoaid.model.InventoryItem;
+import com.alpha5.autoaid.repository.InventryItemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class StockService {
+
+    @Autowired
+    private InventryItemRepository inventryItemRepository;
+
+    public InventryStockRespond inventryItemStock(String itemName){
+        InventoryItem item = inventryItemRepository.findByItemName(itemName);
+
+        if(item == null){
+            throw new RuntimeException("Item Not Available");
+        }else{
+            InventryStockRespond response = new InventryStockRespond();
+            response.setItemNo(item.getItemNo());
+            response.setStock(item.getStock());
+            response.setPrice(item.getPrice());
+
+            return response;
+        }
+
+
+    }
+
+
+}
