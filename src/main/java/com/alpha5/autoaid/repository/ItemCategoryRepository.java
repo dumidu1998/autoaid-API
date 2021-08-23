f222077c903900674dcfc765665ddabc843f4d24
dumidu1998 <dumiduraj@gmail.com>
2021-08-24 02:37:25 +0530

    cat repo added

---
+++ src/main/java/com/alpha5/autoaid/repository/ItemCategoryRepository.java
@@ -0,0 +1,8 @@
+package com.alpha5.autoaid.repository;
+
+import com.alpha5.autoaid.model.ItemCategory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ItemCategoryRepository extends JpaRepository<ItemCategory,Long> {
+
+}
