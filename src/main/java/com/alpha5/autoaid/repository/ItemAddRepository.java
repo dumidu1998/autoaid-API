d94bf66365d032d37d1057ffcd4af467f84fb706
Madhawa Dissanayake <73326224+MadhawaDissanayake@users.noreply.github.com>
2021-09-06 13:01:42 +0530

    add item repository updated

---
+++ src/main/java/com/alpha5/autoaid/repository/ItemAddRepository.java
@@ -0,0 +1,7 @@
+package com.alpha5.autoaid.repository;
+
+import com.alpha5.autoaid.model.ItemAdd;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ItemAddRepository extends JpaRepository<ItemAdd,Long> {
+}
