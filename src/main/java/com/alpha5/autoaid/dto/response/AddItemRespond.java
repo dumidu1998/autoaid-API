ab1cc2f6d83b26dc8947a1f8b1795b515d80fcbe
Madhawa Dissanayake <73326224+MadhawaDissanayake@users.noreply.github.com>
2021-09-06 13:01:10 +0530

    add item respond updated

--- src/main/java/com/alpha5/autoaid/dto/response/AddItemRespond.java
+++ src/main/java/com/alpha5/autoaid/dto/response/AddItemRespond.java
@@ -1,9 +1,19 @@
 package com.alpha5.autoaid.dto.response;
 
 public class AddItemRespond {
+    private long itemId;
     private String name;
 
-    public AddItemRespond(String name) {
+    public long getItemId() {
+        return itemId;
+    }
+
+    public void setItemId(long itemId) {
+        this.itemId = itemId;
+    }
+
+    public AddItemRespond(long itemId, String name) {
+        this.itemId = itemId;
         this.name = name;
     }
 
