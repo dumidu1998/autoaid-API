2676976cd1da6e73529a173907fdc76a6b764b9e
dumidu1998 <dumiduraj@gmail.com>
2021-09-21 21:43:13 +0530

    MANTHI CHANEG

--- src/main/java/com/alpha5/autoaid/repository/RepairRepository.java
+++ src/main/java/com/alpha5/autoaid/repository/RepairRepository.java
@@ -1,6 +1,7 @@
 package com.alpha5.autoaid.repository;
 
 import com.alpha5.autoaid.enums.RepairStatus;
+import com.alpha5.autoaid.enums.RepairType;
 import com.alpha5.autoaid.model.Repair;
 import com.alpha5.autoaid.model.Vehicle;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -13,4 +14,5 @@ public interface RepairRepository extends JpaRepository<Repair, Long> {
     List<Repair> findAllByStaff_StaffIdAndStatusIsNot(long staffId,RepairStatus status);
     List<Repair> findAllByStatusAndVehicleVehicleId(RepairStatus status,long vehicleId);
     List<Repair> findAllByStatusAndVehicle_Customer_UserData_id(RepairStatus status,long userId);
+    List<Repair> findAllByVehicle_VehicleIdAndRepairTypeOrderByRepairAddedDateDesc(long vehicleId, RepairType type);
 }
