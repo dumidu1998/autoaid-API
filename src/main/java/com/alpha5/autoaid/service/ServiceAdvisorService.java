37b2c0fd6472d100f0cb7cd0ececbee15735bf70
Madhawa Dissanayake <73326224+MadhawaDissanayake@users.noreply.github.com>
2021-09-22 19:55:16 +0530

    Updated

--- src/main/java/com/alpha5/autoaid/service/ServiceAdvisorService.java
+++ src/main/java/com/alpha5/autoaid/service/ServiceAdvisorService.java
@@ -427,9 +427,4 @@ public class ServiceAdvisorService {
         return getAllRepairInstanceResponses;
     }
 
-    public Customer getCustomerbyVehicle(long vehicleId){
-        Customer customer = vehicleRepository.findByVehicleId(vehicleId).getCustomer();
-        return customer;
-    }
-
 }
