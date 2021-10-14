f06920b2fb4ef7c2f9bd8fe3075a5551178f97cb
Dumidu Kasun Bandara Rajakaruna <50117273+dumidu1998@users.noreply.github.com>
2021-10-14 12:04:30 +0530

    Update SectionRepository.java

--- src/main/java/com/alpha5/autoaid/repository/SectionRepository.java
+++ src/main/java/com/alpha5/autoaid/repository/SectionRepository.java
@@ -9,5 +9,5 @@ public interface SectionRepository extends JpaRepository<Section, Long> {
     Section findBySectionId(long sectionId);
     Section findByStaff(Staff staff);
     Section findByStaff_UserData_id(long userid);
-    List<Section> findAllByStaff(Staff staff);
+//     List<Section> findAllByStaff(Staff staff);
 }
